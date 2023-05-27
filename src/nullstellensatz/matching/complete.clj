a725a1cc3d17872147e9d23d315629bde53d483c
Brahayan Xavier <brahayan@live.com>
2023-05-27 14:10:49 -0500

    feat: add reducible structures

--- src/nullstellensatz/matching/complete.clj
+++ src/nullstellensatz/matching/complete.clj
@@ -1,1 +1,51 @@
 (ns nullstellensatz.matching.complete)
+
+(defn- generate-odd-numbers [n]
+  (range 1 n 2))
+
+(defn- multiply-values [odd-numbers]
+  (letfn [(->values [[k :as acc] n]
+            (-> k nil? (if 1 k) (* n) (cons acc)))]
+    (reduce ->values [] odd-numbers)))
+
+(defn- generate-code [p products representation]
+  (cond
+    (empty? products) (cons 0 representation)
+    :else (let [k (first products)
+                x (rem p k)
+                y (quot p k)]
+            (recur x (rest products) (cons y representation)))))
+
+(defn- update-structure [[free open] structure]
+  (letfn [(->new-structure [pair]
+            (if (some #{open} pair)
+              (->> pair
+                   (filter #(not= % open))
+                   (cons free)) pair))]
+    (map (comp set ->new-structure) structure)))
+
+
+(defn- generate-structure [representation pairs structure]
+  (cond
+    (empty? representation) structure
+    :else (let [open-item (first representation)
+                [free-item close-item] (first pairs)
+                new-pair (set [open-item close-item])
+                new-structure (cons new-pair (update-structure [free-item open-item] structure))]
+            (recur (rest representation) (rest pairs) new-structure))))
+
+(defn ->size [n]
+  (reduce * 1 (generate-odd-numbers n)))
+
+(defn ->code [nucleotids position]
+  (let [products (-> nucleotids
+                     generate-odd-numbers multiply-values rest)]
+    (generate-code position products [])))
+
+(defn ->structure [nucleotids position]
+  (let [a (range 0 nucleotids 2)
+        b (range 1 nucleotids 2)
+        representation (->code nucleotids position)
+        pairs (map vector a b)]
+    ((comp set generate-structure) representation pairs [])))
+
--- test/nullstellensatz/matching/complete_test.clj
+++ test/nullstellensatz/matching/complete_test.clj
@@ -1,7 +1,22 @@
 (ns nullstellensatz.matching.complete-test
-  (:require [clojure.test :refer :all]
-            [nullstellensatz.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [nullstellensatz.matching.complete :as complete]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest check->code
+  (is (= [0 0 0 0] (complete/->code 8 0)))
+  (is (= [0 2 4 1] (complete/->code 8 29)))
+  (is (= [0 1 2 2] (complete/->code 8 37)))
+  (is (= [0 2 4 6] (complete/->code 8 104))))
+
+(deftest check->size
+  (is (= 15 (complete/->size 6)))
+  (is (= 105 (complete/->size 8)))
+  (is (= 945 (complete/->size 10)))
+  (is (= 10395 (complete/->size 12))))
+
+(deftest check->structure
+  (is (= #{#{0, 7}, #{1, 2}, #{3, 4}, #{5, 6}} (complete/->structure 8 0)))
+  (is (= #{#{0, 6}, #{1, 7}, #{2, 3}, #{4, 5}} (complete/->structure 8 29)))
+  (is (= #{#{0, 4}, #{1, 3}, #{2, 7}, #{5, 6}} (complete/->structure 8 37)))
+  (is (= #{#{0, 1}, #{2, 3}, #{4, 5}, #{6, 7}} (complete/->structure 8 104))))
+
