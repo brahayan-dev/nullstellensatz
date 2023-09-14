b4eb7cbc59eeedf1159f3bccf69344e96da22a5f
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2023-09-14 10:36:54 -0500

    feat: get size for some cases [1]

--- src/nullstellensatz/labeled_graph/connected.clj
+++ src/nullstellensatz/labeled_graph/connected.clj
@@ -1,15 +1,20 @@
 (ns nullstellensatz.labeled-graph.connected
-  (:require [clojure.math.combinatorics :as math]))
+  (:require [clojure.math.combinatorics :refer [count-combinations]]))
 
 (defrecord BasicPack [k-value
                       p-k-value
                       subset-value
                       binomial-value])
 
+(defrecord NumericPack [k-quantity
+                        p-k-quantity
+                        subset-value
+                        binomial-value])
+
 (defn- ->subset-value [k] (->> k (Math/pow 2) dec int))
 
 (defn- ->binomial-value [p k]
-  (let [->combinations #(->> k dec (math/count-combinations %))]
+  (let [->combinations #(->> k dec (count-combinations %))]
     (-> p (- 2) range ->combinations)))
 
 (defn- ->packs [k-values p]
@@ -18,15 +23,31 @@
                              (- p k)
                              (->subset-value k)
                              (->binomial-value p k)))]
-  (map ->pack k-values)))
+    (map ->pack k-values)))
 
-(defn- generate-packs [p]
+(defn- ->basic-packs [p]
   (let [->values #(if (= 1 %) '(1) (range 1 %))]
     (-> p ->values (->packs p))))
 
+(defn- ->g-quantity [n _packs]
+  (let [->treat #(%)
+        ->cache (memoize #(cond
+                            (< % 3) 1
+                            :else (->treat %)))]
+    (->cache n)))
+
+(defn- ->numeric-packs [packs]
+  (let [->translated #(NumericPack. (-> % :k-value (->g-quantity packs))
+                                    (-> % :p-k-value (->g-quantity packs))
+                                    (:subset-value %) (:binomial-value %))]
+    (map ->translated packs)))
+
 (defn ->size [p]
   (let [->product #(->> % vals (apply *'))]
-    (->> p generate-packs (map ->product) (apply +'))))
+    (->> p ->basic-packs ->numeric-packs (map ->product) (apply +'))))
 
-(->size 3)
-(generate-packs 4)
+(comment (let [p 1]
+           (clojure.pprint/pprint {:size (->size p)
+                                   :basic-packs (->basic-packs p)
+                                   :g-quantity (->g-quantity 0 [])
+                                   :numeric-packs ((comp ->numeric-packs ->basic-packs) p)})))
