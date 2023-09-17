3c26e5363e3b9823ddb93407808d3c642e0128e3
Brahayan Xavier <brahayanxavier@Brahayans-MacBook-Pro.local>
2023-09-17 00:22:39 -0500

    feat: enumerate labeled connected graphs

--- src/nullstellensatz/labeled_graph/connected.clj
+++ src/nullstellensatz/labeled_graph/connected.clj
@@ -1,53 +1,63 @@
 (ns nullstellensatz.labeled-graph.connected
   (:require [clojure.math.combinatorics :refer [count-combinations]]))
 
-(defrecord BasicPack [k-value
-                      p-k-value
-                      subset-value
-                      binomial-value])
+(defrecord Pack [n-value
+                 k-value
+                 p-k-value
+                 subset-value
+                 binomial-value])
 
-(defrecord NumericPack [k-quantity
-                        p-k-quantity
-                        subset-value
-                        binomial-value])
+(defrecord Structure [])
 
-(defn- ->subset-value [k] (->> k (Math/pow 2) dec int))
+(def ^:private state (atom {:numeric {}
+                            :constructive {}}))
+
+(defn- debug-log
+  ([x]
+   (debug-log "X" x))
+  ([title x]
+   (let [t (str "|---|" title "|-------------|")]
+     (clojure.pprint/pprint t)
+     (clojure.pprint/pprint x)
+     (clojure.pprint/pprint t) x)))
+
+(defn- ->>state [k i v]
+    (swap! state assoc-in [k i] v) v)
+
+(defn- ->subset-value [k] (->> k (Math/pow 2) dec bigint))
 
 (defn- ->binomial-value [p k]
   (let [->combinations #(->> k dec (count-combinations %))]
     (-> p (- 2) range ->combinations)))
 
-(defn- ->packs [k-values p]
+(defn- ->packs [n k-values]
   (let [->pack (fn [k]
-                 (BasicPack. k
-                             (- p k)
-                             (->subset-value k)
-                             (->binomial-value p k)))]
-    (map ->pack k-values)))
+                 (Pack. n k
+                        (- n k)
+                        (->subset-value k)
+                        (->binomial-value n k)))]
+    (mapv ->pack k-values)))
 
-(defn- ->basic-packs [p]
+(defn- ->basic-store [n]
   (let [->values #(if (= 1 %) '(1) (range 1 %))]
-    (-> p ->values (->packs p))))
+    (->> n ->values vec (->packs n) (->>state :basic n))))
 
-(defn- ->g-quantity [n _packs]
-  (let [->treat #(%)
-        ->cache (memoize #(cond
-                            (< % 3) 1
-                            :else (->treat %)))]
-    (->cache n)))
+(defn- ->quantity [{:keys [n-value k-value p-k-value subset-value binomial-value]}]
+  (if (< n-value 3)
+    (->>state :numeric n-value 1)
+    (let [k-quantity (get-in @state [:numeric k-value])
+          p-k-quantity (get-in @state [:numeric p-k-value])]
+      (*' k-quantity p-k-quantity subset-value binomial-value))))
 
-(defn- ->numeric-packs [packs]
-  (let [->translated #(NumericPack. (-> % :k-value (->g-quantity packs))
-                                    (-> % :p-k-value (->g-quantity packs))
-                                    (:subset-value %) (:binomial-value %))]
-    (map ->translated packs)))
+(defn- ->numeric-store [packs]
+  (->> packs
+       (map ->quantity)
+       (apply +')
+       (->>state :numeric (get-in packs [0 :n-value]))))
 
-(defn ->size [p]
-  (let [->product #(->> % vals (apply *'))]
-    (->> p ->basic-packs ->numeric-packs (map ->product) (apply +'))))
+(defn ->size [n]
+  (let [->answer (comp ->numeric-store ->basic-store)]
+    (->> n inc (range 1) (mapv ->answer) last)))
 
-(comment (let [p 1]
-           (clojure.pprint/pprint {:size (->size p)
-                                   :basic-packs (->basic-packs p)
-                                   :g-quantity (->g-quantity 0 [])
-                                   :numeric-packs ((comp ->numeric-packs ->basic-packs) p)})))
+(comment (let [p 90] (->size p)))
+(comment (clojure.pprint/pprint (:numeric @state)))
