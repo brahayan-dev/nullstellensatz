313bc7032ee58c12421c13df4cc1883ab422303c
Brahayan Xavier <bx@Brahayans-MacBook-Pro.local>
2023-11-01 23:15:44 -0500

    feat: add enumerate and encode in set partition

--- src/nullstellensatz/set_partition.clj
+++ src/nullstellensatz/set_partition.clj
@@ -5,14 +5,16 @@
 (defn- ->ints-from-zero-to-val [n]
   (range 0 n))
 
+(defn ->binomial [n k]
+  (combination/enumerate (dec n) k))
+
 (defn- enumerate-worker [n answer]
-  (let [->binomial #(combination/enumerate (dec n) %)]
-    (if (< n 2) 1
-        (->> n
-             ->ints-from-zero-to-val
-             (map ->binomial)
-             (map *' answer)
-             (apply +')))))
+  (if (< n 2) 1
+      (->> n
+           ->ints-from-zero-to-val
+           (map #(->binomial n %))
+           (map *' answer)
+           (apply +'))))
 
 (defn- enumerate-helper [n k-vals answer]
   (if (empty? k-vals)
@@ -25,3 +27,20 @@
 (defn enumerate [n]
   (let [k-vals (->ints-from-zero-to-val n)]
     (first (enumerate-helper n k-vals []))))
+
+(defn- find-term [n index position]
+  (let [binomial-val (->binomial n position)
+        aux-val (* binomial-val (enumerate position))]
+    (if (>= aux-val index) [position index]
+        (recur n (- index aux-val) (inc position)))))
+
+(defn encode [n index]
+  (let [[k-val tail] (find-term n index 0)
+        bell-val (enumerate k-val)
+        binomial-val (quot tail bell-val)
+        previous-bell-val (rem tail bell-val)]
+    [n k-val binomial-val previous-bell-val]))
+
+
+
+
--- test/nullstellensatz/set_partition_test.clj
+++ test/nullstellensatz/set_partition_test.clj
@@ -1,8 +1,17 @@
 (ns nullstellensatz.set-partition-test
-  (:require [nullstellensatz.set-partition :as set-partition]
-[clojure.test :refer [testing is deftest]]            ))
+  (:require
+   [clojure.test :refer [deftest is]]
+   [matcher-combinators.test :refer [match?]]
+   [nullstellensatz.set-partition :as set-partition]))
 
-(deftest name-test
-      (testing "Context of the test assertions"
-        (is (= 1 (set-partition/enumerate 1))))) 
+(deftest check-enumerate
+  (is (= 1 (set-partition/enumerate 0)))
+  (is (= 1 (set-partition/enumerate 1)))
+  (is (= 2 (set-partition/enumerate 2)))
+  (is (= 5 (set-partition/enumerate 3)))
+  (is (= 15 (set-partition/enumerate 4)))
+  (is (= 52 (set-partition/enumerate 5))))
 
+(deftest check-encode
+  (is (match? [4 2 0 1] (set-partition/encode 4 5)))
+  (is (match? [4 3 0 3] (set-partition/encode 4 13))))
