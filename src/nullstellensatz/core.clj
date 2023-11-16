76a3d7554f44f516980ab009e61d90ecffc89ac8
Xavier Ramirez <xavier.ramirez@nubank.com.br>
2023-11-16 15:10:29 -0500

    feat: change flags for CLI

--- src/nullstellensatz/core.clj
+++ src/nullstellensatz/core.clj
@@ -5,13 +5,17 @@
   (:gen-class))
 
 (def cli-options
-  [["-m" "--molecule MOLECULE" "Molecule ID to load from data"
+  [["-n" "--space ZERO OR POSITIVE INTEGER" "Kind of object to be generated or enumerated"
     :validate [transaction/molecule-exists? "Must be a molecule created within /data"]]
-   ["-s" "--structure STRUCTURE" "Structure number ID to create"
+   ["-i" "--index POSITIVE INTEGER" "Structure number ID to create"
     :default 0
     :parse-fn #(Long/parseLong %)
     :validate [#(< 0 %) "Must be a number between 0 and 9223372036854775807"]]
-   ["-i" "--irreducible"]
+   ["-d" "--complete-linked-diagram"]
+   ["-c" "--noncrossing-linked-diagram"]
+   ["-s" "--irreducible-linked-diagram"]
+   ["-g" "--labeled-connected-graph"]
+   ["-b" "--set-partition"]
    ["-h" "--help"]])
 
 (defn- ->input [args]
