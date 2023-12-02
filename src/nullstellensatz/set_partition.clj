6139b4522f698b01e62cd0961b406bf95e2eee19
Brahayan Xavier <bx@Brahayans-MacBook-Pro.local>
2023-12-02 00:26:00 -0500

    feat: add code based at D&D

--- src/nullstellensatz/set_partition.clj
+++ src/nullstellensatz/set_partition.clj
@@ -1,70 +1,41 @@
-(ns nullstellensatz.set-partition
-  (:require
-   [nullstellensatz.common :as common]
-   [nullstellensatz.combination :as combination]))
+(ns nullstellensatz.set-partition)
 
-(defn ->binomial [n k]
-  (combination/enumerate (dec n) k))
+(defn- ->restricted-growth-row [n i j answer]
+  (if (> j (- n i)) answer
+      (let [value (+ (get-in answer [(dec i) (inc j)])
+                     (* j (get-in answer [(dec i) j])))]
+        (recur n i
+               (inc j)
+               (-> answer (assoc-in [i j] value))))))
 
-(defn ->binomial-object [n k index]
-  (combination/generate (dec n) k (inc index)))
-
-(defn- enumerate-worker [n answer]
-  (if (< n 2) 1
-      (->> n
-           common/->ints-from-zero-to-val-less-one
-           (map #(->binomial n %))
-           (map *' answer)
-           (apply +'))))
-
-(defn- enumerate-helper [n k-vals answer]
-  (if (empty? k-vals)
-    (-> n (enumerate-worker answer) (cons answer))
-    (let [k (first k-vals)
-          tail (rest k-vals)
-          updated-answer (-> k (enumerate-worker answer) (cons answer))]
-      (recur n tail updated-answer))))
+(defn- ->restricted-growth-table [n]
+  (loop [i 1 answer [(-> n inc (repeat 1) vec)]]
+    (if (> i n) answer
+        (recur (inc i)
+               (->restricted-growth-row n i 0 (conj answer []))))))
 
 (defn enumerate [n]
-  (let [k-vals (common/->ints-from-zero-to-val-less-one n)]
-    ((comp first enumerate-helper) n k-vals [])))
+  (let [table (->restricted-growth-table n)]
+    (get-in table [n 0])))
 
-(defn- find-term [n index position]
-  (let [binomial-val (->binomial n position)
-        term-val (*' binomial-val (enumerate position))]
-    (if (> term-val index) [position index]
-        (recur n (-' index term-val) (inc position)))))
+(defn unrank [n r]
+  (let [table (->restricted-growth-table n)]
+    (loop [i 2 j 1 r r answer {1 1}]
+      (if (> i n) answer
+          (let [d (get-in table [(- n i) j])
+                jump? (>= r (* j d))
+                ->add #(assoc answer i %)]
+            (recur (inc i)
+                   (if jump? (inc j) j)
+                   (if jump? (- r (* j d)) (mod r d))
+                   (if jump?
+                     (->add (inc j))
+                     (->add (inc (quot r d))))))))))
 
-(defn encode [n index]
-  (let [[k-val tail] (find-term n index 0)
-        bell-val (enumerate k-val)
-        binomial-val (quot tail bell-val)
-        previous-bell-val (rem tail bell-val)]
-    [n k-val binomial-val previous-bell-val]))
-
-(defn wrap [n i]
-  (loop [n n index i answer []]
-    (let [[_ k-val _ previous-bell-val :as code] (encode n index)
-          updated-answer (cons code answer)]
-      (if (< n 2) updated-answer
-          (recur k-val previous-bell-val updated-answer)))))
-
-(defn- update-block [binomial]
-  (let [->updated-block #(get binomial (dec %) %)]
-    (fn [block] (mapv ->updated-block block))))
-
-(defn exist? [v item]
-  ((comp not nil? some) #{item} v))
-
-(defn- ->object [previous-partition [n k-val binomial-val _]]
-  (case n
-    0 previous-partition
-    1 (cons [1] previous-partition)
-    (let [binomial (->binomial-object n k-val binomial-val)
-          block (remove #(exist? binomial %) (common/->ints-from-one-to-val n))]
-      (-> binomial update-block (map previous-partition) (conj block)))))
-
-(defn search-by-codes [wrapping]
-  (reduce ->object [] wrapping))
-
-(def search (comp search-by-codes wrap))
+(defn search [n r]
+  (let [code (unrank n r)
+        structure (as-> code $
+                    (vals $)
+                    (apply max $)
+                    (repeat $ []) (vec $))]
+    (reduce (fn [s [k v]] (update s (dec v) conj k)) structure code)))
--- test/nullstellensatz/set_partition_test.clj
+++ test/nullstellensatz/set_partition_test.clj
@@ -12,33 +12,28 @@
   (is (= 15 (set-partition/enumerate 4)))
   (is (= 52 (set-partition/enumerate 5))))
 
-(deftest check-encode
-  (is (match? [0 0 0 0] (set-partition/encode 0 0)))
-  (is (match? [1 0 0 0] (set-partition/encode 1 0)))
-  (is (match? [2 0 0 0] (set-partition/encode 2 0)))
-  (is (match? [2 1 0 0] (set-partition/encode 2 1)))
-  (is (match? [3 0 0 0] (set-partition/encode 3 0)))
-  (is (match? [3 1 0 0] (set-partition/encode 3 1)))
-  (is (match? [3 1 1 0] (set-partition/encode 3 2)))
-  (is (match? [3 2 0 0] (set-partition/encode 3 3)))
-  (is (match? [3 2 0 1] (set-partition/encode 3 4)))
-  (is (match? [4 2 0 1] (set-partition/encode 4 5)))
-  (is (match? [4 3 0 3] (set-partition/encode 4 13))))
-
-(deftest check-wrap
-  (is (match? [[1 0 0 0] [2 1 0 0] [4 2 0 1]] (set-partition/wrap 4 5)))
-  (is (match? [[0 0 0 0] [2 0 0 0] [3 2 0 0] [4 3 0 3]] (set-partition/wrap 4 13))))
-
-(deftest check-exist?
-  (is (= true (set-partition/exist? [1 2 3] 3)))
-  (is (= false (set-partition/exist? [1 2 3] 5))))
+(deftest check-unrank
+  (is (match? {1 1 2 1 3 1 4 1} (set-partition/unrank 4 0)))
+  (is (match? {1 1 2 1 3 1 4 2} (set-partition/unrank 4 1)))
+  (is (match? {1 1 2 1 3 2 4 1} (set-partition/unrank 4 2)))
+  (is (match? {1 1 2 1 3 2 4 2} (set-partition/unrank 4 3)))
+  (is (match? {1 1 2 1 3 2 4 3} (set-partition/unrank 4 4)))
+  (is (match? {1 1 2 2 3 1 4 1} (set-partition/unrank 4 5)))
+  (is (match? {1 1 2 2 3 1 4 2} (set-partition/unrank 4 6)))
+  (is (match? {1 1 2 2 3 1 4 3} (set-partition/unrank 4 7)))
+  (is (match? {1 1 2 2 3 2 4 1} (set-partition/unrank 4 8)))
+  (is (match? {1 1 2 2 3 2 4 2} (set-partition/unrank 4 9)))
+  (is (match? {1 1 2 2 3 2 4 3} (set-partition/unrank 4 10)))
+  (is (match? {1 1 2 2 3 3 4 1} (set-partition/unrank 4 11)))
+  (is (match? {1 1 2 2 3 3 4 2} (set-partition/unrank 4 12)))
+  (is (match? {1 1 2 2 3 3 4 3} (set-partition/unrank 4 13)))
+  (is (match? {1 1 2 2 3 3 4 4} (set-partition/unrank 4 14))))
 
 (deftest check-search
   (is (match? [[1 2 3]] (set-partition/search 3 0)))
-  (is (match? [[2 3] [1]] (set-partition/search 3 1)))
+  (is (match? [[1 2] [3]] (set-partition/search 3 1)))
   (is (match? [[1 3] [2]] (set-partition/search 3 2)))
-  (is (match? [[3] [1 2]] (set-partition/search 3 3)))
-  (is (match? [[3] [2] [1]] (set-partition/search 3 4)))
-  (is (match? [[3 4] [2] [1]] (set-partition/search 4 5)))
-  (is (match? [[4] [3] [1 2]] (set-partition/search 4 13))))
-
+  (is (match? [[1] [2 3]] (set-partition/search 3 3)))
+  (is (match? [[1] [2] [3]] (set-partition/search 3 4)))
+  (is (match? [[1 3 4] [2]] (set-partition/search 4 5)))
+  (is (match? [[1] [2] [3 4]] (set-partition/search 4 13))))
