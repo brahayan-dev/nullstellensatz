90d8e6df4c2d2a7dcfc742bc1f365e5e7ac28929
Brahayan Xavier <bx@Brahayans-MacBook-Pro.local>
2023-12-02 18:10:40 -0500

    refactor: enhance algorithm design

--- src/nullstellensatz/labeled_connected_graph.clj
+++ src/nullstellensatz/labeled_connected_graph.clj
@@ -1,103 +1,20 @@
 (ns nullstellensatz.labeled-connected-graph
   (:require
-   [nullstellensatz.combination :as combination]
-   [nullstellensatz.subset :as subset]))
+   [clojure.math :refer [pow]]
+   [nullstellensatz.combination :as combination]))
 
-(defrecord Pack [n-value
-                 k-value
-                 n-k-value
-                 subset-value
-                 binomial-value])
+(defn- ->updated-cache [i cache]
+  (loop [k 1 answer []]
+    (if (> k (dec i))
+      (assoc cache i (apply +' answer))
+      (let [first-val (get cache k)
+            second-val (get cache (-' i k))
+            label-val ((comp int dec pow) 2 k)
+            binomial-val (combination/enumerate (-' i 2) (dec k))
+            result-val (*' binomial-val label-val first-val second-val)]
+        (recur (inc k) (cons result-val answer))))))
 
-(defrecord Structure [n-value k-value labels nodes])
-
-(def ^:private empty-state {:packs {}
-                            :sizes {}
-                            :outcomes {}})
-
-(def ^:private state (atom empty-state))
-
-(defn- ->>state [field index v]
-  (swap! state assoc-in [field index] v) v)
-
-(defn- state->>
-  ([field]
-   (get @state field))
-  ([field index]
-   (get-in @state [field index]))
-  ([field index default]
-   (get-in @state [field index]
-           (->>state field index default))))
-
-(defn ->items [n]
-  (if (= 1 n) '(1) (range 1 n)))
-
-(defn ->packs [n]
-  (letfn [(->subset [k]
-            (->> k subset/enumerate dec))
-          (->binomial [p k]
-            (combination/enumerate (- p 2) (dec k)))
-          (->answer [k]
-            (state->> :packs [n k]
-                      (Pack. n k
-                             (- n k)
-                             (->subset k)
-                             (->binomial n k))))]
-    (->> n ->items (mapv ->answer))))
-
-(defn- ->sizes [n]
-  (state->> :sizes n
-            (->> :outcomes
-                 state->>
-                 (into [])
-                 (filter (fn [[[n-value _] _]] (= n-value n)))
-                 (map (fn [[_ v]] v))
-                 (reduce +))))
-
-(defn- ->outcomes [{:keys [n-value k-value n-k-value subset-value binomial-value]}]
-  (state->> :outcomes [n-value k-value]
-            (if (< n-value 3) 1
-                (let [k-outcome (->sizes k-value)
-                      n-k-outcome (->sizes n-k-value)]
-                  (*' k-outcome n-k-outcome subset-value binomial-value)))))
-
-(defn ->quantities [n]
-  (->> n
-       inc
-       ->items
-       (mapv ->packs)
-       flatten
-       (mapv ->outcomes)))
-
-(defn enumerate [n] (->quantities n) (->sizes n))
-
-(defn- find-term [n m]
-  (loop [n n m m k 1]
-    (let [outcome (state->> :outcomes [n k])
-          jump? (< m outcome)
-          m_ (- m outcome)
-          k_ (inc k)]
-      (if (not jump?) [k_ m_]
-          (recur n (if jump? m_ m) k_)))))
-
-(defn find-labels [n k m]
-  (let [outcome (state->> :outcomes [n k])
-        m_ (-> m (quot outcome) inc)]
-    [(combination/generate (- n 2) (dec k) m_) (rem m outcome)]))
-
-(defn find-nodes [n k m]
-  (let [{:keys [subset-value n-k-value]} (state->> :polynomials [n k])
-        g-1 (enumerate k)
-        g-2 (enumerate n-k-value)
-        m_ (->> (*' subset-value g-1 g-2) (quot m) inc)]
-    [(subset/generate (dec n) m_) (rest m m_)]))
-
-(defn generate [n m]
-  (let [_ (->quantities n)
-        [k m-1] (find-term n m)
-        [labels m-2] (find-labels n k m-1)
-        [nodes _m-3] (find-nodes n k m-2)]
-    (Structure. n k labels nodes)))
-
-(comment (clojure.pprint/pprint (generate 4 2)))
-(comment (clojure.pprint/pprint @state))
+(defn enumerate [n]
+  (loop [i 2 cache {1 1}]
+    (if (> i n) (get cache n)
+        (recur (inc i) (->updated-cache i cache)))))
--- test/nullstellensatz/labeled_connected_graph_test.clj
+++ test/nullstellensatz/labeled_connected_graph_test.clj
@@ -4,53 +4,6 @@
    [matcher-combinators.test :refer [match?]]
    [nullstellensatz.labeled-connected-graph :as connected]))
 
-(def polynomials
-  {1 [{:n-value 1 :k-value 1 :n-k-value 0 :subset-value 1 :binomial-value 1}]
-   2 [{:n-value 2 :k-value 1 :n-k-value 1 :subset-value 1 :binomial-value 1}]
-   3 [{:n-value 3 :k-value 1 :n-k-value 2 :subset-value 1 :binomial-value 1}
-      {:n-value 3 :k-value 2 :n-k-value 1 :subset-value 3 :binomial-value 1}]
-   4 [{:n-value 4 :k-value 1 :n-k-value 3 :subset-value 1 :binomial-value 1}
-      {:n-value 4 :k-value 2 :n-k-value 2 :subset-value 3 :binomial-value 2}
-      {:n-value 4 :k-value 3 :n-k-value 1 :subset-value 7 :binomial-value 1}]
-   5 [{:n-value 5 :k-value 1 :n-k-value 4 :subset-value 1 :binomial-value 1}
-      {:n-value 5 :k-value 2 :n-k-value 3 :subset-value 3 :binomial-value 3}
-      {:n-value 5 :k-value 3 :n-k-value 2 :subset-value 7 :binomial-value 3}
-      {:n-value 5 :k-value 4 :n-k-value 1 :subset-value 15 :binomial-value 1}]})
-
-(def results
-  "[[1 1] 1
-    [2 1] 1
-    [3 1] 1  [3 2] 3
-    [4 1] 4  [4 2] 6  [4 3] 28
-    [5 1] 38 [5 2] 36 [5 3] 84 [5 4] 570]"
-  {1 [1]
-   2 [1 1]
-   3 [1 1 1 3]
-   4 [1 1 1 3 4 6 28]
-   5 [1 1 1 3 4 6 28 38 36 84 570]})
-
-(deftest check->items
-  (is (match? [1] (connected/->items 1)))
-  (is (match? [1] (connected/->items 2)))
-  (is (match? [1 2] (connected/->items 3)))
-  (is (match? [1 2 3] (connected/->items 4)))
-  (is (match? [1 2 3 4] (connected/->items 5)))
-  (is (match? [1 2 3 4 5] (connected/->items 6))))
-
-(deftest check->packs
-  (is (match? (get polynomials 1) (connected/->packs 1)))
-  (is (match? (get polynomials 2) (connected/->packs 2)))
-  (is (match? (get polynomials 3) (connected/->packs 3)))
-  (is (match? (get polynomials 4) (connected/->packs 4)))
-  (is (match? (get polynomials 5) (connected/->packs 5))))
-
-(deftest check->quantities
-  (is (match? (get results 1) (connected/->quantities 1)))
-  (is (match? (get results 2) (connected/->quantities 2)))
-  (is (match? (get results 3) (connected/->quantities 3)))
-  (is (match? (get results 4) (connected/->quantities 4)))
-  (is (match? (get results 5) (connected/->quantities 5))))
-
 (deftest check-enumerate
   (is (= 1 (connected/enumerate 1)))
   (is (= 1 (connected/enumerate 2)))
@@ -58,4 +11,3 @@
   (is (= 38 (connected/enumerate 4)))
   (is (= 728 (connected/enumerate 5)))
   (is (= 26704 (connected/enumerate 6))))
-
