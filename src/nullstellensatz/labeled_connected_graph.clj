201b29ef32235785f65d3777cbfd80bc916998ef
Brahayan Xavier <bx@Brahayans-MacBook-Pro.local>
2023-12-05 21:37:36 -0500

    feat: add search of k-val in G-objects

--- src/nullstellensatz/labeled_connected_graph.clj
+++ src/nullstellensatz/labeled_connected_graph.clj
@@ -1,30 +1,44 @@
 (ns nullstellensatz.labeled-connected-graph
   (:require
    [clojure.math :refer [pow]]
-   [nullstellensatz.combination :as combination]))
+   [nullstellensatz.combination :as combination]
+   [nullstellensatz.labeled-connected-graph :as connected]))
+
+(defn- ->term [n k cache]
+  (let [first-val (get cache k)
+        second-val (get cache (-' n k))
+        label-val ((comp int dec pow) 2 k)
+        binomial-val (combination/enumerate (-' n 2) (dec k))]
+    (*' binomial-val label-val first-val second-val)))
 
 (defn- ->updated-cache [i cache]
   (loop [k 1 answer []]
     (if (> k (dec i))
       (assoc cache i (apply +' answer))
-      (let [first-val (get cache k)
-            second-val (get cache (-' i k))
-            label-val ((comp int dec pow) 2 k)
-            binomial-val (combination/enumerate (-' i 2) (dec k))
-            result-val (*' binomial-val label-val first-val second-val)]
-        (recur (inc k) (cons result-val answer))))))
+      (let [value (->term i k cache)]
+        (recur (inc k) (cons value answer))))))
 
 (defn enumerate [n]
   (loop [i 2 cache {1 1}]
     (if (> i n) (get cache n)
         (recur (inc i) (->updated-cache i cache)))))
 
-(defn ->k-val [n r]
-  (loop [r r k-val nil answer [n]]
-    (if (-> k-val nil? not) {:result r
-                             :answer (cons k-val answer)}
-        (let [jump? (nil? k-val)
-              k (if jump? r (-' r value))]
-          (recur k
-                 (>= k value)
-                 (if jump? (cons k answer) answer))))))
+(defn- ->location [n r]
+  (loop [k 1 r r cache {1 1}]
+    (let [p (- n k)
+          can-update? #(not (contains? cache %))
+          ->cache (fn [c i] (assoc c i (enumerate i)))
+          updated-cache (cond-> cache
+                          (can-update? k) (->cache k)
+                          (can-update? p) (->cache p))
+          value (->term n k updated-cache)]
+      (if (< r value) {:r r :k k :n n :cache updated-cache}
+          (recur (inc k) (- r value) updated-cache)))))
+
+(defn- ->tag [{:keys [remainder answer]}]
+  ())
+
+(defn- ->node [{:keys [remainder answer]}]
+  ())
+
+(def unrank (comp ->node ->tag ->location))
