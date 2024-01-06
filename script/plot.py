92334b183d57d5f5df33e0c3c5f670f97eac94d6
Brahayan Xavier <bx@Brahayans-MacBook-Pro.local>
2024-01-06 09:09:00 -0500

    feat: enhance plot script

--- script/plot.py
+++ script/plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import json
 import argparse
 import matplotlib.pyplot as plt
@@ -9,18 +10,45 @@ def extract_means(data):
     return [record["mean"] for record in data["results"]]
 
 
-def plot(means):
+def get_labels(object_type):
+    return {
+        "c": ("Dyck Paths", "Nodes in the path"),
+        "b": ("Set Partitions", "Items in the set"),
+        "d": ("Complete Linked Diagrams", "Arcs in the diagram"),
+        "g": ("Labeled Connected Graphs", "Points in the graph"),
+        "s": ("Irreducible Linked Diagrams", "Arcs in the diagram"),
+    }[object_type]
+
+
+def plot(means, object_type, algorithm_type):
     indexes = list(range(1, len(means) + 1))
+    name_label, x_label = get_labels(object_type)
 
     plt.figure(figsize=(8, 6))
-    plt.plot(indexes, means, marker="o", linestyle="-", color="b")
+    plt.plot(indexes, means, ".", color="#ff3399")
 
-    plt.title("Algorithm behavior to generate LCG")
-    plt.xlabel("Points in the graph")
+    plt.title("Algorithm behavior to " + algorithm_type + " " + name_label)
+    plt.xlabel(x_label)
     plt.ylabel("Time (seconds)")
-    plt.grid(True)
+    plt.grid(True, color="#e0e0e0", linestyle="--", linewidth=1)
 
     plt.show()
+
+
+def get_algorithm_type(path):
+    match = re.search(r"([a-z]+)/([a-z]+)", path)
+    if match != None:
+        return match.group(2)
+    else:
+        return None
+
+
+def get_object_type(path):
+    match = re.search(r"\d+([a-z]+)\d+", path)
+    if match != None:
+        return match.group(1)
+    else:
+        return None
 
 
 def load_json(path_to_file):
@@ -31,7 +59,9 @@ def load_json(path_to_file):
 def main(path):
     data = load_json(path)
     means = extract_means(data)
-    plot(means)
+    object_type = get_object_type(path)
+    algorithm_type = get_algorithm_type(path)
+    plot(means, object_type, algorithm_type)
 
 
 if __name__ == "__main__":
