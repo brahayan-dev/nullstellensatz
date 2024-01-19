f3401f854c94ec2ccb8377ae9105c66bc564e777
Xavier Ramirez <xavier.ramirez@nubank.com.br>
2024-01-19 17:14:37 -0500

    feat: add support for fixed size for k-subsets

--- src/nullstellensatz/core.clj
+++ src/nullstellensatz/core.clj
@@ -5,14 +5,18 @@
   (:gen-class))
 
 (def cli-options
-  [["-n" "--space ZERO OR POSITIVE INTEGER" "Kind of object to be generated or enumerated"
+  [["-n" "--space ZERO OR POSITIVE INTEGER" "Size of object to be generated or enumerated"
     :parse-fn #(Long/parseLong %)
     :validate [#(<= 0 %) "Must be a number between 0 and 9223372036854775807"]]
    ["-m" "--index ZERO OR POSITIVE INTEGER" "Structure number ID to create"
     :parse-fn #(Long/parseLong %)
     :validate [#(<= 0 %) "Must be a number between 0 and 9223372036854775807"]]
-   ["-o" "--object ONE OPTION OF: 'b', 'c', 'd', 'g' or 's'"
-    :validate [#(some #{%} ["b" "c" "d" "g" "s"]) "Must be selected one of: 'b', 'c', 'd', 'g' or 's'"]]
+   ["-k" "--fixed-size ZERO OR POSITIVE INTEGER" "Fixed size of subset to be generated or enumerated in combinations (f)"
+    :default 0
+    :parse-fn #(Long/parseLong %)
+    :validate [#(<= 0 %) "Must be a number between 0 and 9223372036854775807"]]
+   ["-o" "--object ONE OPTION OF: 'a', 'f', 'b', 'c', 'd', 'g' or 's'" "Type of object to be generated or enumerated"
+    :validate [#(some #{%} ["a" "f" "b" "c" "d" "g" "s"]) "Must be selected one of: 'a', 'f', 'b', 'c', 'd', 'g' or 's'"]]
    ["-h" "--help"]])
 
 (defn- ->input [args]
--- src/nullstellensatz/transaction.clj
+++ src/nullstellensatz/transaction.clj
@@ -1,13 +1,12 @@
 (ns nullstellensatz.transaction
   (:require
    [nullstellensatz.object.catalan-family :as noncrossing-linked-diagram]
+   [nullstellensatz.object.combination :as combination]
    [nullstellensatz.object.complete-linked-diagram :as complete-linked-diagram]
    [nullstellensatz.object.irreducible-linked-diagram :as irreducible-linked-diagram]
    [nullstellensatz.object.labeled-connected-graph :as labeled-connected-graph]
-   [nullstellensatz.object.set-partition :as set-partition]))
-
-#_(defn- ->file [name]
-    (io/file "data" (str name ".edn")))
+   [nullstellensatz.object.set-partition :as set-partition]
+   [nullstellensatz.object.subset :as subset]))
 
 (defn- ->answer [{:keys [generated size index]}]
   (when size (if index generated size)))
@@ -16,10 +15,13 @@
   (when-not (empty? errors)
     (first errors)) input)
 
-(defn- add-size [{:keys [space object errors] :as input}]
+(defn- add-size [{:keys [fixed-size space object errors] :as input}]
   (if (empty? errors)
     (let [n space
+          k fixed-size
           size (case object
+                 "a" (subset/enumerate n)
+                 "f" (combination/enumerate n k)
                  "b" (set-partition/enumerate n)
                  "g" (labeled-connected-graph/enumerate n)
                  "d" (complete-linked-diagram/enumerate n)
@@ -27,10 +29,13 @@
                  "s" (irreducible-linked-diagram/enumerate n))]
       (assoc input :size size)) input))
 
-(defn- fetch-object [{:keys [index object space errors] :as input}]
+(defn- fetch-object [{:keys [fixed-size index object space errors] :as input}]
   (if (and index (empty? errors))
     (let [n space
+          k fixed-size
           generated (case object
+                      "a" (subset/generate n)
+                      "f" (combination/generate n k index)
                       "b" (set-partition/generate n index)
                       "g" (labeled-connected-graph/generate n index)
                       "d" (complete-linked-diagram/generate n index)
@@ -44,6 +49,12 @@
                          "\": A structure must be between 0 and " size)]
       (assoc input :errors [error-msg])) input))
 
+(defn- validate-fixed-size [{:keys [size index errors fixed-size] :as input}]
+  (if (and fixed-size (> fixed-size size) (empty? errors))
+    (let [error-msg (str "Failed to validate \"-k " index
+                         "\": A fixed k-subset must be between 0 and " size)]
+      (assoc input :errors [error-msg])) input))
+
 (defn- flat-data [{:keys [options errors summary]}]
   (assoc options :errors errors :summary summary))
 
@@ -52,5 +63,6 @@
                ->errors
                fetch-object
                validate-index
+               validate-fixed-size
                add-size
                flat-data))
