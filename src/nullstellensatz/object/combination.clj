fefac198d58dfcc0e98957f8577e7ef6394eec70
Xavier Ramirez <xavier.ramirez@nubank.com.br>
2024-04-09 09:30:02 -0500

    refactor: clear hint types

--- src/nullstellensatz/object/combination.clj
+++ src/nullstellensatz/object/combination.clj
@@ -1,22 +1,22 @@
 (ns nullstellensatz.object.combination)
 
-(defn- factorial ^Long [^Long n]
+(defn- factorial [n]
   (loop [i 2 acc 1]
     (if (or (<= n 1) (> i n)) acc
         (recur (inc i) (*' acc i)))))
 
-(defn- rising-factorial ^Long [^Long n ^Long k]
+(defn- rising-factorial [n k]
   (loop [i 0 acc 1]
     (if (> i (dec k)) acc
         (recur (inc i) (*' acc (-' n i))))))
 
-(defn enumerate ^Long [^Long n ^Long k]
+(defn enumerate [n k]
   (if (neg? k) 0
       (let [k-val (factorial k)
             p-val (rising-factorial n k)]
         (quot p-val k-val))))
 
-(defn generate [^Long n ^Long k ^Long i]
+(defn generate [n  k  i]
   (loop [n n k k index i answer []]
     (if (or (zero? n) (zero? k)) (vec answer)
         (let [n_ (dec n)
