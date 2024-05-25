3941e654c4069614420f095c71afca3c5312c219
Brahayan Xavier <brahayan@live.com>
2024-05-25 13:22:48 -0500

    feat: clean up!

--- src/nullstellensatz/algorithm/find.clj
+++ src/nullstellensatz/algorithm/find.clj
@@ -1,16 +1,14 @@
 (ns nullstellensatz.algorithm.find)
 
-(defn- helper [items n answer]
-  (if (zero? n) answer
+(defn- helper [items answer]
+  (if (empty? items) answer
       (let [[head & tail] items]
         (recur tail
-               (count tail)
                (if (> head answer) head answer)))))
 
 (defn ->maximum [items]
   (cond
     (nil? items) nil
     (empty? items) nil
-    :else (let [[head & tail] items
-                n (count tail)]
-            (helper tail n head))))
+    :else (let [[head & tail] items]
+            (helper tail head))))
