39fc62550a28a0d0fdd02c9a9316eefc1057f659
Brahayan Xavier <bxsr@proton.me>
2026-09-04 20:00:13 -0500

    chore: add renamed

--- src/nullstellensatz/algorithm/sort.clj
+++ src/nullstellensatz/algorithm/sort.clj
@@ -3,14 +3,12 @@
 (defn standard [items]
   (sort items))
 
-(defn- quick-helper [items]
+(defn quick [items]
   (if (<= (count items) 1)
     items
     (let [pivot (first items)
           tail (rest items)
           smaller (filter #(< % pivot) tail)
           larger (filter #(>= % pivot) tail)]
-      (concat (quick-helper smaller) [pivot] (quick-helper larger)))))
+      (concat (quick smaller) [pivot] (quick larger)))))
 
-(defn quick [items]
-  (quick-helper items))
